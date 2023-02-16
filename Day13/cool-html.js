d0da15c3ba175304199485480a1cadbc4dd490ca
chiragmahawar7 <cmahawar_be16@thapar.edu>
2023-02-16 23:46:47 +0530

    Day 13 - cool html elements

---
+++ Day13/cool-html.js
@@ -0,0 +1,9 @@
+function increaseMeter() {
+    var meter = document.getElementById('meter');
+    meter.value += 5;
+}
+
+function decreaseMeter() {
+    var meter = document.getElementById('meter');
+    meter.value -= 5;
+}
