0ebf797f13ddc5e7f10fbd0410d6e435bc32c2e7
chiragmahawar7 <chiragmahawar000@gmail.com>
2024-07-03 00:52:24 +0530

    js - array callback with parametrized function

--- Day12/weird-javascript.js
+++ Day12/weird-javascript.js
@@ -13,3 +13,16 @@ console.log('11' - 1); // prints 10
 // while addition, first operand is type casted to string. '11' & '1' are concatenated to '111'
 // while subtraction, first operand is type casted to number and then subtraction is performed bw 11 & 1
 // to avoid such situations use toString() and parseInt()
+
+// 3. array callback functions
+const result = [1,4,9].map(Math.sqrt);
+console.log(result);  // 1,2,3
+
+const result1 = ['1','4','9'].map(parseInt);
+console.log(result1);  // [1, NaN, NaN]
+
+// callback is invoked with three arguments:
+// the value of the element, the index of the element, and the Array object being traversed.
+
+// So if you call a function parseInt which actually expects two arguments,
+// the second argument will be the index of the element
