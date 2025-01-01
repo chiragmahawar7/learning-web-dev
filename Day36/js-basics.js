f6f64e1f4839ad417d781a00fa9c2a27a1dd7854
chiragmahawar7 <chiragmahawar000@gmail.com>
2025-01-01 23:25:15 +0530

    Day 36 - JS basics

---
+++ Day36/js-basics.js
@@ -0,0 +1,9 @@
+// Javascript -  synchronous single-threaded language/
+// everything in JS happens inside an execution context.
+
+// Execution Context - 2 components
+// 1. Memory component- where all variables & functions are stored as key value pairs
+// aka variable environment
+
+// code component - where code is executed line by line
+// aka thread of execution
