456cdbe2d37c1529c7939cf19c5ba635424eb992
chiragmahawar7 <chiragmahawar000@gmail.com>
2025-01-08 00:57:04 +0530

    understanding undefined

---
+++ Day36/undefined.js
@@ -0,0 +1,8 @@
+// Undefined
+// A variable that has not been assigned a value is of type undefined
+// it is a special keyword which takes up its own memory but is a placeholder which is kept
+// for the time bieng until that variable is assigned value.
+
+console.log(a); // undefined
+var a = 7;
+console.log(a); // 7
